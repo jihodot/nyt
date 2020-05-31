9cde03d9ace60958c1806b9927af138c69a53f14
jihod <jihodot@gmail.com>
2020-05-31 22:41:32 +0900

    [feat] add more feature ver1
    add features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PropTypes from "prop-types";
+function Food({name, picture, rating}){
+  return <div>
+    <h1>I like {name}</h1>
+    <h1>rating {rating}</h1>
+    <img src={picture} alt={name}/>
+    </div>;
 }
+
+const foodILike = [
+  {
+    id:1,
+    name: "kimch",
+    image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/10/10/e1d38d22a01a5f11619e141e089f66cb1.jpg",
+    rating:1000
+  },
+  {
+    id:2,
+    name: "yoon dahye",
+    image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/10/10/e1d38d22a01a5f11619e141e089f66cb1.jpg",
+    rating:10000
+  },
+  {
+    id:3,
+    name: "song jiho",
+    image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/10/10/e1d38d22a01a5f11619e141e089f66cb1.jpg",
+    rating:1
+  }
+]
+
+
+function renderFood(dish){
+  console.log(dish);
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+}
+
+Food.propTypes={
+
+}
+function App() {
+  return <div>
+      {foodILike.map(renderFood)}
+    </div>
+};
 
 export default App;
